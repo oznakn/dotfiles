4dee79ff92773e84a09997d11294e37780ca6e70
Ozan Akın <ozn.akn@gmail.com>
2019-09-17 16:31:44 +0300

    arch version, improvements, change terminal

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,12 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
-	if [[ $(xrandr --query | grep "HDMI2 connected primary" | wc -l) == 1 ]]; then
-  		MONITOR="eDP1" TRAYPOS="right" polybar --reload main &
-	elif [[ $(xrandr --query | grep " connected" | cut -d " " -f1 | wc -w) == 2 ]]; then
-		MONITOR="eDP1"  TRAYPOS="right" polybar --reload main &
+	if [[ $(xrandr --query | grep "HDMI-2 connected primary" | wc -l) == 1 ]]; then
+  		MONITOR="HDMI-2" TRAYPOS="right" polybar --reload main &
+  		#   MONITOR="eDP-1" TRAYPOS="none" polybar --reload main &
+	elif [[ $(xrandr --query | grep " eDP-1 connected primary" | wc -l) == 1 ]]; then
+		MONITOR="eDP-1"  TRAYPOS="right" polybar --reload main &
 	else
-  		MONITOR="eDP1" TRAYPOS="right" polybar --reload main &
+  		MONITOR="eDP-1" TRAYPOS="right" polybar --reload main &
 	fi
 
 fi
