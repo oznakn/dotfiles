8f9fce657f12459b56fa3f4562f5d8af1df460be
Ozan Akın <ozn.akn@gmail.com>
2019-10-19 01:26:41 +0300

    updates, shortcuts, add gnome keyring

--- update.sh
+++ update.sh
@@ -2,14 +2,20 @@
 
 find . -not -path "**/.git/*" -not -name ".git" -not -path "./update.sh" -delete
 
+mkdir -p ssh
+
 cp -r ~/.config/polybar .
 cp -r ~/.config/bspwm .
 cp -r ~/.config/sxhkd .
 cp -r ~/.config/alacritty .
 cp -r ~/.config/fish .
+cp -r ~/.config/gtk-3.0 .
 
 cp ~/.config/compton.conf .
+
+cp ~/.ssh/config ssh/
 
 cp ~/.vimrc .
 cp ~/.tmux.conf .
 cp ~/.xinitrc .
+cp ~/.gtkrc-2.0 .
